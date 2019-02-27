2cb0d5fc6665f626ca32982fa695d50d93ea3fa2
babbea74 <43272969+babbea74@users.noreply.github.com>
2019-02-27 11:55:43 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,8 +12,8 @@ fi
 vnc4server
 export DISPLAY=$HOSTNAME:1
 ./run_SARWIND_LGMod_v401.sh /MCR_R2011b/v716 ./InData/SARWIND_IO_Info.txt ./InData/SARWIND_Settings.txt
-ls ./ > text
-zip -r OutData.zip OutData
+ls ./OutData > text
+gzip -r OutData.zip OutData
 find ./OutData -name *_Results.txt -exec mv {} . \;
 find . -name *_Results.txt -exec mv {} SARWIND_LGMod_Results.txt \;
 cat  SARWIND_LGMod_Results.txt text >  SARWIND_LGMod_Results.txt
